75d46dd156c413573063481cdf5e4ce263038704
sungyunkim <sungk1003@naver.com>
2016-09-28 10:59:22 +0900

    admin관리

--- src/Main.java
+++ src/Main.java
@@ -3,20 +3,9 @@ import banana_admin.controller.Controllers;
 public class Main {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
+		
 		new Controllers();
-		Controllers.getUserController().requestAllUser();
 
-=======
-	
-		new Controllers();
-		Controllers.getAdminController().requestSelectAllAdmin();
-		Controllers.getAdminController().requestDelete();
-		Controllers.getAdminController().requestUpdateAdminNumber();
-		Controllers.getAdminController().menuViewToInsertView(); // insert
-		
-		
->>>>>>> refs/remotes/origin/pr/2
 	}
 
 }
--- src/banana_admin/controller/Controllers.java
+++ src/banana_admin/controller/Controllers.java
@@ -3,50 +3,37 @@ package banana_admin.controller;
 public class Controllers {
 
 	private static ProgramController programController;
-<<<<<<< HEAD
 	private static EmotionController emotionController;
 	private static UserController userController;
-=======
 	private static AdminController adminController;
 	
->>>>>>> refs/remotes/origin/pr/2
 	public Controllers() {
 
 		programController = new ProgramController();
-<<<<<<< HEAD
 		userController=new UserController();
-
 		emotionController = new EmotionController();
-
-=======
 		adminController = new AdminController();
 		
->>>>>>> refs/remotes/origin/pr/2
 	}
 
 	public static ProgramController getProgramController() {
 
 		return programController;
-
 	}
 
-<<<<<<< HEAD
 	public static UserController getUserController() {
+		
 		return userController;
 	}
 
-
 	public static EmotionController getEmotionController() {
+		
 		return emotionController;
 	}
 
-
-=======
 	public static AdminController getAdminController() {
 		
 		return adminController;
-		
 	}
 
->>>>>>> refs/remotes/origin/pr/2
 }
