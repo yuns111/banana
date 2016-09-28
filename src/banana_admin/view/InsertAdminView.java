9500d193dbeedd03a66639310f1526c66148d7bd
gonomujasic <gonomujasic@hotmail.com>
2016-09-28 11:22:54 +0900

    ddd
    ddd

--- src/banana_admin/view/InsertAdminView.java
+++ src/banana_admin/view/InsertAdminView.java
@@ -2,16 +2,11 @@ package banana_admin.view;
 
 import java.util.Scanner;
 
-<<<<<<< HEAD
-public class InsertAdminView {
-	
-=======
 import banana_admin.controller.Controllers;
 import banana_admin.domain.AdminDomain;
 
 public class InsertAdminView {
 
->>>>>>> refs/heads/test-branch
 	private Scanner keyboard;
 	
 	public InsertAdminView() {
@@ -22,21 +17,6 @@ public class InsertAdminView {
 	
 	public void insertAdminView(){
 		
-<<<<<<< HEAD
-		System.out.println("아이디을 입력하세요.");
-		String insertID = keyboard.next();
-		System.out.println("비밀번호를 입력하세요.");
-		String insertPassword = keyboard.next();
-		System.out.println("관리자명을 입력하세요.");
-		String insertAdminName = keyboard.next();
-		
-		
-		
-		
-	}
-	
-
-=======
 		System.out.println("관리자 아이디을 입력하세요.");
 		String insertAdminId = keyboard.next();
 		System.out.println("관리자 비밀번호를 입력하세요.");
@@ -50,5 +30,4 @@ public class InsertAdminView {
 	}
 	
 	
->>>>>>> refs/heads/test-branch
 }
