7566197c1e0760b864ea1ed83c4a7c0d8e42d1a1
gonomujasic <gonomujasic@hotmail.com>
2016-09-28 18:22:33 +0900

    commit

--- src/banana_admin/controller/Controllers.java
+++ src/banana_admin/controller/Controllers.java
@@ -3,15 +3,11 @@ package banana_admin.controller;
 public class Controllers {
 
 	private static ProgramController programController;
-	private static AdminController adminController;
 	private static TicketController ticketController;
 	private static EmotionController emotionController;
 	private static UserController userController;
-<<<<<<< HEAD
-=======
 	private static MusicController musicController;
 	private static AdminController adminController;
->>>>>>> refs/remotes/yuns111/master
 	private static LoginController loginController;
 
 	public Controllers() {
@@ -19,16 +15,11 @@ public class Controllers {
 		ticketController = new TicketController();
 		programController = new ProgramController();
 		userController = new UserController();
-<<<<<<< HEAD
-=======
 		musicController = new MusicController();
->>>>>>> refs/remotes/yuns111/master
 		emotionController = new EmotionController();
 		adminController = new AdminController();
 		loginController = new LoginController();
 	}
-
-	
 	
 	public static TicketController getTicketController() {
 		return ticketController;
